59694db487f7ab9fd063dbca13f248191423e08c
DoronLaadan <laadan1235@gmail.com>
2017-06-12 10:15:43 +0300

    starting out

--- src/View/MazeDisplay.java
+++ src/View/MazeDisplay.java
@@ -1,9 +1,32 @@
 package View;
 
+import algorithms.mazeGenerators.Maze;
+import algorithms.mazeGenerators.Position;
 import javafx.scene.canvas.Canvas;
 
 /**
  * Created by user on 11/06/2017.
  */
 public class MazeDisplay extends Canvas {
+
+    private Maze maze;
+
+    private Position heroPosition;
+
+    public void setMaze(Maze newMaze){
+        maze=newMaze;
+        //redrawMaze()
+    }
+
+    public void setHeroPosition(Position newHeroPosition){
+        heroPosition=newHeroPosition;
+        //redrawHero()
+    }
+
+    public Position getHeroPosition() {
+        return heroPosition;
+    }
+
+
+
 }
